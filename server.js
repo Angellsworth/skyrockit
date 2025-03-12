f97e81ad4a65fc0ce44e31a2f4e84cd1e9b4fd96
Angellsworth <angellsworth@msn.com>
2025-03-12 13:01:09 -0600

    wip: added applications.js to controllers folder

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-const authController = require('./controllers/auth.js')
 const isSignedIn = require('./middleware/is-signed-in.js')
 const passUserToView = require('./middleware/pass-user-to-view.js')
+
+const authController = require('./controllers/auth.js')
+const applicationsController = require('./controllers/applications.js')
+
 
 const authController = require('./controllers/auth.js');
 
@@ -47,7 +50,7 @@ app.use('/auth', authController);
 //user has to sign in first before they can qualify as signedIn
 app.use(isSignedIn)
 //isSignIn runs AFTER auth routes
-
+app.use('/users/:userId/applications', applicationsController)
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
