c20ecac512479ec5a75cc18e2161f38adf9c33ad
Angellsworth <angellsworth@msn.com>
2025-03-12 16:18:00 -0600

    wip: had corupted data

--- controllers/applications.js
+++ controllers/applications.js
@@ -14,5 +14,20 @@ router.get('/', async (req, res) => {
     }
   });
 
+//GET /users/:userId/application/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs')
+})
 
+router.post('/', async (req,res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id)
+    currentUser.applications.push(req.body)//changes the applications list in memory only
+    await currentUser.save();//makes the changes permanent in the database
+    res.redirect(`/users/${currentUser._id}/applications`)
+  }catch(error){
+    console.log(error)
+    res.redirect('/')
+  }
+})
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -3,18 +3,27 @@ const mongoose = require("mongoose");
 // Define the User schema first
 // Define the Application schema separately (if you intended to use it)
 const applicationSchema = new mongoose.Schema({
-  company: { type: String, required: true },
-  title: { type: String, required: true },
-  notes: { type: String },
-  postingLink: { type: String },
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
   status: {
     type: String,
-    enum: ["interested", "applied", "interviewing", "rejected", "accepted"],
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
   },
 });
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String },
+  username: { type: String, required: true},
   password: { type: String, required: true }, // Store hashed password
   applications: [ // Embedding applications as an array of objects
    applicationSchema
